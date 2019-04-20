2ae3b5ed4efc596cece0f6f0be7cb9e3528f36ad
lihbr <lihbr@users.noreply.github.com>
2019-04-20 20:15:08 +0200

    :sparkles: init netlify cms

---
+++ pages/admin/_pass.vue
@@ -0,0 +1,28 @@
+<template>
+  <div />
+</template>
+
+<script>
+import sha256 from "sha256";
+
+export default {
+  validate({ params }) {
+    // console.log(sha256.x2(process.env.ADMIN_URL)); // use it to know how to rename the admin config folder
+    return params.pass === process.env.ADMIN_URL;
+  },
+  head() {
+    return {
+      link: [
+        {
+          href: `admin/${sha256.x2(process.env.ADMIN_URL)}/config.yml`,
+          type: "text/yaml",
+          rel: "cms-config-url"
+        }
+      ],
+      script: [
+        { src: "https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js" }
+      ]
+    };
+  }
+};
+</script>
