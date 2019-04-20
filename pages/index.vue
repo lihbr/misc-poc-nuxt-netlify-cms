a5115912c1e06aa354ed031c230ab739faa5c6c1
edhbr <edhbr@users.noreply.github.com>
2019-04-20 18:29:39 +0200

    :tada: initial commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    home
+  </div>
+</template>
+
+<script>
+// import Component from '~/components/Component.vue'
+
+export default {
+  components: {
+    // Component
+  }
+};
+</script>
+
+<style scoped lang="stylus"></style>
